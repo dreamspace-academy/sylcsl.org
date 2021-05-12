7c9cd1d009eb09e471161f1e4fa8aa9f50394bef
GunaRakulan-GR <gunarakulan@gmail.com>
2021-05-12 11:57:46 +0530

    Update to dev

---
+++ car_main.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>          //Servo motor library. This is standard library
+#include <NewPing.h>        //Ultrasonic sensor function library. You must install this library
+
+
+//our L298N control pins
+const int LeftMotorForward = 7;
+const int LeftMotorBackward = 6;
+const int RightMotorForward = 5;
+const int RightMotorBackward = 4;
+
+
+void setup() {
+
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+
+}
+
+void loop() {
+
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+    delay(3000);
+
+    digitalWrite(LeftMotorForward, LOW);
+    digitalWrite(RightMotorForward, LOW);
+    delay(3000);
+
+    digitalWrite(LeftMotorBackward, HIGH);
+    digitalWrite(RightMotorBackward, HIGH);
+    delay(3000);
+
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
+    delay(3000);
+    
+}
